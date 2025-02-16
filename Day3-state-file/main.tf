eeb58658998cc41082c018ca3b5aeac5e53ad16c
Lavanya8978 <lavanyaeega57@gmail.com>
2025-02-16 19:23:58 +0530

    terraform day-3 practice

--- Day3-state-file/main.tf
+++ Day3-state-file/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "name" {
     ami = "ami-085ad6ae776d8f09c"
     instance_type = "t3.micro"
     availability_zone="us-east-1a"
+    tags{
+        name="testserver"
+    }
     
 }
 
